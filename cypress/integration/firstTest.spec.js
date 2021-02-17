cae1b6fc47045979d070e5bfd0343ef31e027c38
ilya <i.arcer@reverse.systems>
2021-02-17 13:02:34 +0500

    1

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -16,31 +16,3 @@ it('EXPECT', ()=>{
         })
        
 })
-
-it('check default value for Deposits', ()=>{
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.get('[data-qa-value="UAH"]')
-        .should('be.checked')
-})
-
-it('check is visible Archive link', ()=>{
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.contains('Мої депозити')
-        .trigger('mouseover')
-        .get('#archiveDeposits')
-        .should('be.visible')
-})
-
-it('check is correct attr in button', ()=>{
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.contains('Show cards')
-        .should('have.attr', 'type')
-        .and('match', /button/)
-})
-
-it.only('check is correct URL', ()=>{
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.url()
-        .should('eq', 'https://next.privat24.ua/?lang=en')
-})
-//1
